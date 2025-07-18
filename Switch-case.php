ec2643db0b832299958d5b6f224a3bb55c79f7eb
Abhishek Jaiswal <100372101+Codewiththinkabhi@users.noreply.github.com>
2025-07-18 23:58:22 +0530

    Create Switch-case.php

---
+++ Switch-case.php
@@ -0,0 +1,17 @@
+<?php
+$day = "Tuesday";
+
+switch ($day) {
+    case "Monday":
+        echo "Today is Monday.";
+        break;
+    case "Tuesday":
+        echo "Today is Tuesday.";
+        break;
+    case "Wednesday":
+        echo "Today is Wednesday.";
+        break;
+    default:
+        echo "It's another day.";
+}
+?>
